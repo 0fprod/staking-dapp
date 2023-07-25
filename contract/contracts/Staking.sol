ea39601b7f00601a1e50b848b59604be3b1e33a6
0fprod <franp.dev@pm.me>
2023-07-26 00:37:32 +0100

    refactor: improve code security and update compound logic

--- contract/contracts/Staking.sol
+++ contract/contracts/Staking.sol
@@ -1,41 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.11;
 
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
 import {console} from "hardhat/console.sol";
 
-error Staking__InsufficientAllowance();
-error Staking__InsufficientBalance();
 error Staking__InsufficientStakedBalance();
 error Staking__InsufficientContractBalance();
+error Staking__InsufficientTimePassed();
 
 contract Staking {
+    using SafeERC20 for IERC20;
+    using SafeMath for uint256;
+
     struct Staker {
         uint256 stakedAmount;
-        uint256 stakedAt;
+        uint256 lastUpdated;
     }
 
-    uint constant APY = 5; // 5 %
-    IERC20 public Token;
-
+    IERC20 public immutable Token;
+    uint constant APY = 5;
+    uint public totalStaked = 0;
     mapping(address => Staker) public stakers;
 
     constructor(address _token) {
         Token = IERC20(_token);
-    }
-
-    modifier notEnoughAllowance(uint256 _amount) {
-        if (Token.allowance(msg.sender, address(this)) < _amount) {
-            revert Staking__InsufficientAllowance();
-        }
-        _;
-    }
-
-    modifier notEnoughBalance(uint256 _amount) {
-        if (_amount > Token.balanceOf(msg.sender)) {
-            revert Staking__InsufficientBalance();
-        }
-        _;
     }
 
     modifier notEnoughStakedBalance(uint256 _amount) {
@@ -48,60 +38,67 @@ contract Staking {
         _;
     }
 
-    function fundContractWithErc20Token(
-        uint256 _amount
-    ) public notEnoughAllowance(_amount) notEnoughBalance(_amount) {
+    function fundContractWithErc20Token(uint256 _amount) public {
         Token.transferFrom(msg.sender, address(this), _amount);
     }
 
-    function getContractBalance() public view returns (uint256) {
-        return Token.balanceOf(address(this));
+    function getAvailableRewards() public view returns (uint256) {
+        return Token.balanceOf(address(this)).sub(totalStaked);
     }
 
-    function stake(
-        uint256 _amount
-    ) public notEnoughAllowance(_amount) notEnoughBalance(_amount) {
+    function getStakedAmount() public view returns (uint256) {
+        return totalStaked;
+    }
+
+    function stake(uint256 _amount) public {
         if (stakers[msg.sender].stakedAmount == 0) {
-            stakers[msg.sender].stakedAt = block.timestamp;
+            stakers[msg.sender].lastUpdated = block.timestamp;
         }
         Token.transferFrom(msg.sender, address(this), _amount);
-        stakers[msg.sender].stakedAmount += _amount;
+        stakers[msg.sender].stakedAmount = stakers[msg.sender].stakedAmount.add(
+            _amount
+        );
+        totalStaked = totalStaked.add(_amount);
     }
 
     function unstake(uint256 _amount) public notEnoughStakedBalance(_amount) {
         Token.approve(address(this), _amount);
         Token.transferFrom(address(this), msg.sender, _amount);
-        stakers[msg.sender].stakedAmount -= _amount;
+        stakers[msg.sender].stakedAmount = stakers[msg.sender].stakedAmount.sub(
+            _amount
+        );
+        totalStaked = totalStaked.sub(_amount);
     }
 
-    function getStakedAmount(address _address) public view returns (uint256) {
+    function getStakedAmountFor(
+        address _address
+    ) public view returns (uint256) {
         return stakers[_address].stakedAmount;
     }
 
-    function calculateRewards() public view returns (uint) {
-        uint tokenRewardsPerSecond = calculateTokenRewardPerSecond();
-
-        uint secondsStaked = block.timestamp - stakers[msg.sender].stakedAt;
-        uint rewardAmount = secondsStaked * tokenRewardsPerSecond;
-        return rewardAmount;
-    }
-
     function calculateTokenRewardPerSecond() public view returns (uint) {
-        // 5 % of total staked
-        uint totalStaked = stakers[msg.sender].stakedAmount;
-        uint appliedApy = (totalStaked * APY) / 100;
-        uint rewardsPerSecond = appliedApy / 52 weeks;
+        uint stakedBalance = stakers[msg.sender].stakedAmount;
+        uint appliedApy = stakedBalance.mul(APY).div(100);
+        uint rewardsPerSecond = appliedApy.div(52 weeks);
         return rewardsPerSecond;
     }
 
-    function claimReward() public {
-        uint rewardAmount = calculateRewards();
+    function compoundRewards() public {
+        uint timePassed = block.timestamp - stakers[msg.sender].lastUpdated;
+        if (timePassed < 1 weeks) {
+            revert Staking__InsufficientTimePassed();
+        }
 
-        if (rewardAmount > Token.balanceOf(address(this))) {
+        uint rewards = calculateTokenRewardPerSecond();
+        if (getAvailableRewards() < rewards) {
             revert Staking__InsufficientContractBalance();
         }
 
-        Token.approve(address(this), rewardAmount);
-        Token.transferFrom(address(this), msg.sender, rewardAmount);
+        uint rewardAmount = timePassed.mul(rewards);
+        stakers[msg.sender].stakedAmount = stakers[msg.sender].stakedAmount.add(
+            rewardAmount
+        );
+        stakers[msg.sender].lastUpdated = block.timestamp;
+        totalStaked = totalStaked.add(rewardAmount);
     }
 }
--- contract/test/Staking.ts
+++ contract/test/Staking.ts
@@ -24,151 +24,186 @@ describe("Staking contract", function () {
     return { stakingContract, tokenContract, deployerAddress };
   }
 
-  it("is funded with the given amount of tokens", async () => {
-    const { stakingContract, tokenContract, deployerAddress } = await loadFixture(deployFixture);
-    const fiveTokens = tokensAmount(5);
-    const tenTokens = tokensAmount(10);
-    await approveAndFundContract(stakingContract, tokenContract, 5);
+  describe('Deployment', () => {
+    it("is funded with the given amount of tokens", async () => {
+      const { stakingContract, tokenContract } = await loadFixture(deployFixture);
+      const fiveTokens = tokensAmount(5);
+      await approveAndFundContract(stakingContract, tokenContract, 5);
 
-    expect(await stakingContract.getContractBalance()).to.equal(fiveTokens);
-    expect(await tokenContract.balanceOf(stakingContract.address)).to.equal(fiveTokens);
-    expect(await tokenContract.balanceOf(deployerAddress)).to.equal(tenTokens.sub(fiveTokens));
-  });
+      expect(await stakingContract.getAvailableRewards()).to.equal(fiveTokens);
+      expect(await tokenContract.balanceOf(stakingContract.address)).to.equal(fiveTokens);
+    });
 
-  it('reverts tx if sender\'s allowance is not enough when funding', async () => {
-    const { stakingContract } = await loadFixture(deployFixture);
-    const fiveTokens = tokensAmount(5);
+    it('reverts tx if sender\'s allowance is not enough when funding', async () => {
+      const { stakingContract } = await loadFixture(deployFixture);
+      const fiveTokens = tokensAmount(5);
 
-    await expect(stakingContract.fundContractWithErc20Token(fiveTokens))
-      .revertedWithCustomError(stakingContract, "Staking__InsufficientAllowance");
-  });
+      await expect(stakingContract.fundContractWithErc20Token(fiveTokens))
+        .revertedWith('ERC20: insufficient allowance')
+    });
 
-  it('reverts tx if sender\'s balance is not enough when funding', async () => {
-    const { stakingContract, tokenContract } = await loadFixture(deployFixture);
-    const twentyTokens = tokensAmount(20);
+    it('reverts tx if sender\'s balance is not enough when funding', async () => {
+      const { stakingContract, tokenContract } = await loadFixture(deployFixture);
+      const twentyTokens = tokensAmount(20);
 
-    await tokenContract.approve(stakingContract.address, twentyTokens);
+      await tokenContract.approve(stakingContract.address, twentyTokens);
 
-    await expect(stakingContract.fundContractWithErc20Token(twentyTokens))
-      .revertedWithCustomError(stakingContract, "Staking__InsufficientBalance");
-  });
+      await expect(stakingContract.fundContractWithErc20Token(twentyTokens))
+        .revertedWith('ERC20: transfer amount exceeds balance');
+    });
 
-  it("allows to stake tokens", async () => {
-    const { stakingContract, tokenContract, deployerAddress } = await loadFixture(deployFixture);
-    const tenTokens = tokensAmount(10);
-    const fiveTokens = tokensAmount(5);
-    const twoTokens = tokensAmount(2);
-    await approveAndFundContract(stakingContract, tokenContract, 5)
-
-    await approveAndStake(stakingContract, tokenContract, 2)
-
-    expect(await stakingContract.getContractBalance()).to.equal(fiveTokens.add(twoTokens));
-    expect(await tokenContract.balanceOf(deployerAddress)).to.equal(tenTokens.sub(fiveTokens).sub(twoTokens));
-    expect(await stakingContract.getStakedAmount(deployerAddress)).to.equal(twoTokens);
-  });
-
-  it('reverts tx if sender\'s allowance is not enough when staking', async () => {
-    const { stakingContract, tokenContract } = await loadFixture(deployFixture);
-    await approveAndFundContract(stakingContract, tokenContract, 5);
-    const twoTokens = tokensAmount(2);
-
-    await expect(stakingContract.stake(twoTokens))
-      .revertedWithCustomError(stakingContract, "Staking__InsufficientAllowance");
-  });
-
-  it('reverts tx if sender\'s amount is not enough when staking', async () => {
-    const { stakingContract, tokenContract } = await loadFixture(deployFixture);
-    const twentyTokens = tokensAmount(20);
-    const fiveTokens = tokensAmount(5);
-    const sevenTokens = tokensAmount(7);
-    await tokenContract.approve(stakingContract.address, twentyTokens);
-    await stakingContract.fundContractWithErc20Token(fiveTokens)
-
-    await expect(stakingContract.stake(sevenTokens))
-      .revertedWithCustomError(stakingContract, "Staking__InsufficientBalance");
-  });
-
-  it("allows to unstake tokens", async () => {
-    const { stakingContract, tokenContract, deployerAddress } = await loadFixture(deployFixture);
-    const twoTokens = tokensAmount(2)
-    await approveAndFundContract(stakingContract, tokenContract, 5);
-    await approveAndStake(stakingContract, tokenContract, 2)
-
-    await stakingContract.unstake(twoTokens);
-
-    expect(await tokenContract.balanceOf(deployerAddress)).to.equal(tokensAmount(5))
-    expect(await stakingContract.getStakedAmount(deployerAddress)).to.equal(tokensAmount(0))
-  });
-
-  it("revert tx if staker has no balance when unstaking", async () => {
-    const { stakingContract, tokenContract } = await loadFixture(deployFixture);
-    const twoTokens = tokensAmount(2)
-    await approveAndFundContract(stakingContract, tokenContract, 5);
-
-    await expect(stakingContract.unstake(twoTokens))
-      .revertedWithCustomError(stakingContract, "Staking__InsufficientStakedBalance");
+    it('has 0 staked amount', async () => {
+      const { stakingContract } = await loadFixture(deployFixture);
+      expect(await stakingContract.getStakedAmount()).to.equal(tokensAmount(0));
+    });
   })
 
-  it("reverts tx if staker tries to unstake more than the staked amount", async () => {
-    const { stakingContract, tokenContract } = await loadFixture(deployFixture);
-    await approveAndFundContract(stakingContract, tokenContract, 5);
-    await approveAndStake(stakingContract, tokenContract, 2)
+  describe('Stake', () => {
+    it("allows to stake tokens", async () => {
+      const { stakingContract, tokenContract, deployerAddress } = await loadFixture(deployFixture);
+      const twoTokens = tokensAmount(2);
+      await tokenContract.approve(stakingContract.address, twoTokens);
 
-    await stakingContract.unstake(tokensAmount(1));
+      await expect(stakingContract.stake(twoTokens)).to.changeTokenBalances(
+        tokenContract,
+        [deployerAddress, stakingContract.address],
+        [twoTokens.mul(-1), twoTokens]
+      );
+    });
 
-    await expect(stakingContract.unstake(tokensAmount(2)))
-      .revertedWithCustomError(stakingContract, "Staking__InsufficientStakedBalance");
+    it('reverts tx if sender\'s allowance is not enough when staking', async () => {
+      const { stakingContract, tokenContract } = await loadFixture(deployFixture);
+      await approveAndFundContract(stakingContract, tokenContract, 5);
+      const twoTokens = tokensAmount(2);
+
+      await expect(stakingContract.stake(twoTokens))
+        .revertedWith('ERC20: insufficient allowance')
+    });
+
+    it('reverts tx if sender\'s amount is not enough when staking', async () => {
+      const { stakingContract, tokenContract } = await loadFixture(deployFixture);
+      const twentyTokens = tokensAmount(20);
+      const fiveTokens = tokensAmount(5);
+      const sevenTokens = tokensAmount(7);
+      await tokenContract.approve(stakingContract.address, twentyTokens);
+      await stakingContract.fundContractWithErc20Token(fiveTokens)
+
+      await expect(stakingContract.stake(sevenTokens))
+        .revertedWith('ERC20: transfer amount exceeds balance');
+    });
   })
 
-  it('calculates the reward per token based on 5% apy', async () => {
-    // 5 % APY = 0.05 
-    // 1 * (5 / 100) / 1 year in seconds = 0.000000001589845339
-    // With 1 token at 5% makes 0.000000001589845339 every second
-    const tenTokens = 1;
-    const { stakingContract, tokenContract } = await loadFixture(deployFixture);
-    const rewardPerToken = 1589845339
-    await approveAndStake(stakingContract, tokenContract, tenTokens);
+  describe('Unstake', () => {
+    it("allows to unstake tokens", async () => {
+      const { stakingContract, tokenContract, deployerAddress } = await loadFixture(deployFixture);
+      const twoTokens = tokensAmount(2)
+      await approveAndStake(stakingContract, tokenContract, 2)
 
-    expect((await stakingContract.calculateTokenRewardPerSecond())).to.eq(rewardPerToken);
-  });
+      expect(await stakingContract.unstake(twoTokens)).to.changeTokenBalances(
+        tokenContract,
+        [deployerAddress, stakingContract.address],
+        [twoTokens, twoTokens.mul(-1)]
+      );
+      expect(await stakingContract.getStakedAmountFor(deployerAddress)).to.equal(0)
+      expect(await stakingContract.getAvailableRewards()).to.equal(0);
+    });
 
-  it('calculates all the rewards per time staked', async () => {
-    const oneToken = 1;
-    const weeksPassed = 52;
-    const { stakingContract, tokenContract } = await loadFixture(deployFixture);
-    await approveAndStake(stakingContract, tokenContract, oneToken);
-    await moveTimeForwardInWeeks(weeksPassed)
+    it("revert tx if staker has no balance when unstaking", async () => {
+      const { stakingContract, tokenContract } = await loadFixture(deployFixture);
+      const twoTokens = tokensAmount(2)
+      await approveAndFundContract(stakingContract, tokenContract, 5);
 
-    // around 0.05 tokens
-    expect(await stakingContract.calculateRewards()).to.gte(tokensAmount(0.05));
-    expect(await stakingContract.calculateRewards()).to.lte(tokensAmount(0.051));
+      await expect(stakingContract.unstake(twoTokens))
+        .revertedWithCustomError(stakingContract, "Staking__InsufficientStakedBalance");
+    })
+
+    it("reverts tx if staker tries to unstake more than the staked amount", async () => {
+      const { stakingContract, tokenContract } = await loadFixture(deployFixture);
+      await approveAndFundContract(stakingContract, tokenContract, 5);
+      await approveAndStake(stakingContract, tokenContract, 2)
+
+      await stakingContract.unstake(tokensAmount(1));
+
+      await expect(stakingContract.unstake(tokensAmount(2)))
+        .revertedWithCustomError(stakingContract, "Staking__InsufficientStakedBalance");
+    })
   })
 
-  it('allows to claim rewards', async () => {
-    const oneToken = 1;
-    const weeksPassed = 52;
-    const { stakingContract, tokenContract, deployerAddress } = await loadFixture(deployFixture);
-    await approveAndStake(stakingContract, tokenContract, oneToken);
-    await moveTimeForwardInWeeks(weeksPassed)
+  describe('Reward system', () => {
+    it('should calculate rewards per second correctly', async () => {
+      // Test that the contract calculates the rewards per second correctly based on the staked amount.
+      // Stake 10 tokens, expect 5% annual interest, and verify the rewards per second.    
+      const oneWeek = 604800 // 1 week in seconds
+      const tenTokens = 10;
+      const expectedRewardsAfterOneWeek = (tenTokens * 5 / 100) / (oneWeek * 52);
+      const formattedExpectedRewards = expectedRewardsAfterOneWeek.toFixed(18).toString();
+      const { stakingContract, tokenContract } = await loadFixture(deployFixture);
+      await approveAndStake(stakingContract, tokenContract, tenTokens);
 
-    const initialBalance = await tokenContract.balanceOf(deployerAddress);
-    const initialContractBalance = await tokenContract.balanceOf(stakingContract.address);
-    await stakingContract.claimReward();
-    const finalBalance = await tokenContract.balanceOf(deployerAddress);
-    const finalContractBalance = await tokenContract.balanceOf(stakingContract.address);
+      const rewardsPerSecondInWei = await stakingContract.calculateTokenRewardPerSecond()
+      const formattedRewards = ethers.utils.formatUnits(rewardsPerSecondInWei, 18);
 
-    expect(finalBalance).to.gt(initialBalance);
-    expect(finalContractBalance).to.lt(initialContractBalance);
-  });
+      expect(formattedRewards).to.eq(formattedExpectedRewards);
+    });
 
-  it('reverts tx if clamied amount exceeds contract\'s balance', async () => {
-    const oneToken = 1;
-    const weeksPassed = 1040; // 20 years
-    const { stakingContract, tokenContract } = await loadFixture(deployFixture);
-    await approveAndStake(stakingContract, tokenContract, oneToken);
-    await moveTimeForwardInWeeks(weeksPassed)
+    it('should compound rewards and increase balance while reducing available rewards', async () => {
+      // Test that the contract correctly compounds rewards after one week of staking.
+      // Stake 1 token, fund the contract with 5 tokens, and verify balance and available rewards.    
+      const oneToken = 1;
+      const weeksPassed = 1;
+      const { stakingContract, tokenContract, deployerAddress } = await loadFixture(deployFixture);
+      await approveAndFundContract(stakingContract, tokenContract, 5);
+      await approveAndStake(stakingContract, tokenContract, oneToken);
+      await moveTimeForwardInWeeks(weeksPassed)
 
-    await expect(stakingContract.claimReward())
-      .revertedWithCustomError(stakingContract, "Staking__InsufficientContractBalance");
-  });
+      const initialRewards = await stakingContract.getAvailableRewards();
+      const initialBalance = await stakingContract.getStakedAmountFor(deployerAddress);
+      await stakingContract.compoundRewards();
+      const finalRewards = await stakingContract.getAvailableRewards();
+      const finalBalance = await stakingContract.getStakedAmountFor(deployerAddress);
+
+      expect(finalBalance).to.gt(initialBalance);
+      expect(finalRewards).to.lt(initialRewards);
+    });
+
+    it('should compound rewards correctly after one year', async () => {
+      // Test that the contract compounds rewards correctly after one year (52 weeks).
+      // Stake 1 token, expect 5% annual interest, and verify the staked amount after one year.
+      // The expected staked amount should be 1.05 tokens, considering 18 decimal places.    
+      const weeksPassed = 52;
+      const expectedStakedAmount = '1.05';
+      const { stakingContract, tokenContract, deployerAddress } = await loadFixture(deployFixture);
+      await approveAndFundContract(stakingContract, tokenContract, 5);
+      await approveAndStake(stakingContract, tokenContract, 1);
+      await moveTimeForwardInWeeks(weeksPassed)
+
+      await stakingContract.compoundRewards();
+
+      const stakedAmountAfterOneYear = await stakingContract.getStakedAmountFor(deployerAddress);
+      const formattedStakedAmount = ethers.utils.formatUnits(stakedAmountAfterOneYear, 18).substring(0, 4);
+      expect(formattedStakedAmount).to.equal(expectedStakedAmount);
+    })
+
+    it('reverts tx if you try to compound rewards before a week passed', async () => {
+      const oneToken = 1;
+      const weeksPassed = 0;
+      const { stakingContract, tokenContract } = await loadFixture(deployFixture);
+      await approveAndStake(stakingContract, tokenContract, oneToken);
+      await moveTimeForwardInWeeks(weeksPassed)
+
+      await expect(stakingContract.compoundRewards())
+        .revertedWithCustomError(stakingContract, "Staking__InsufficientTimePassed");
+    });
+
+    it('reverts tx if rewards amount exceeds contract\'s balance', async () => {
+      const oneToken = 1;
+      const weeksPassed = 1040; // 20 years
+      const { stakingContract, tokenContract } = await loadFixture(deployFixture);
+      await approveAndStake(stakingContract, tokenContract, oneToken);
+      await moveTimeForwardInWeeks(weeksPassed)
+
+      await expect(stakingContract.compoundRewards())
+        .revertedWithCustomError(stakingContract, "Staking__InsufficientContractBalance");
+    });
+  })
 });
