e322cd081324e9bcbef89951ce58b1dcf40ddf0b
0fprod <franp.dev@pm.me>
2023-08-03 00:17:25 +0100

    add natspec documentation

--- contract/contracts/Staking.sol
+++ contract/contracts/Staking.sol
@@ -28,18 +28,10 @@ contract Staking is Ownable {
         Token = IERC20(_token);
     }
 
-    function fundContractWithErc20Token(uint256 _amount) public {
-        Token.transferFrom(msg.sender, address(this), _amount);
-    }
-
-    function getAvailableRewards() public view returns (uint256) {
-        return Token.balanceOf(address(this)).sub(totalStaked);
-    }
-
-    function getStakedAmount() public view returns (uint256) {
-        return totalStaked;
-    }
-
+    /**
+     * @dev Stakes the given amount of tokens
+     * @param _amount Amount of tokens to stake
+     */
     function stake(uint256 _amount) public {
         if (stakers[msg.sender].stakedAmount == 0) {
             stakers[msg.sender].lastUpdated = block.timestamp;
@@ -51,6 +43,10 @@ contract Staking is Ownable {
         totalStaked = totalStaked.add(_amount);
     }
 
+    /**
+     * @dev Unstakes the given amount of tokens
+     * @param _amountToUnstake Amount of tokens to unstake
+     */
     function unstake(uint256 _amountToUnstake) public {
         Staker memory staker = stakers[msg.sender];
         uint rewardAmount = calculateCompoundedRewards();
@@ -61,20 +57,6 @@ contract Staking is Ownable {
         if (staker.stakedAmount < _amountToUnstake) {
             revert Staking__InsufficientStakedBalance();
         }
-
-        // TODO: Review edge cases
-        // if (
-        //     _amountToUnstake <= stakers[msg.sender].stakedAmount &&
-        //     availableRewards < rewardAmount
-        // ) {
-        //     Token.transfer(msg.sender, _amountToUnstake);
-        //     stakers[msg.sender].stakedAmount = stakers[msg.sender]
-        //         .stakedAmount
-        //         .sub(_amountToUnstake);
-        //     stakers[msg.sender].lastUpdated = block.timestamp;
-        //     totalStaked = totalStaked.sub(_amountToUnstake);
-        //     return;
-        // }
 
         if (availableRewards < rewardAmount) {
             revert Staking__InsufficientContractBalance();
@@ -88,28 +70,10 @@ contract Staking is Ownable {
         totalStaked = virtualTotalStaked.sub(_amountToUnstake);
     }
 
-    function getStakedAmountFor(
-        address _address
-    ) public view returns (uint256) {
-        return stakers[_address].stakedAmount;
-    }
-
-    function calculatePercentageOf(
-        uint amount,
-        uint percentage
-    ) public pure returns (uint) {
-        return
-            ABDKMathQuad.toUInt(
-                ABDKMathQuad.div(
-                    ABDKMathQuad.mul(
-                        ABDKMathQuad.fromUInt(amount),
-                        ABDKMathQuad.fromUInt(percentage)
-                    ),
-                    ABDKMathQuad.fromUInt(100)
-                )
-            );
-    }
-
+    /**
+     * @dev Calculates the compounded rewards for the staker
+     * @return The compounded rewards for the staker
+     */
     function calculateCompoundedRewards() public view returns (uint256) {
         Staker memory staker = stakers[msg.sender];
         uint timeDifference = block.timestamp - staker.lastUpdated;
@@ -124,5 +88,64 @@ contract Staking is Ownable {
         }
 
         return balance.sub(staker.stakedAmount);
+    }
+
+    /**
+     * @dev Calculates the percentage of a number
+     * @param amount The number to calculate the percentage of
+     * @param percentage The percentage to calculate
+     */
+    function calculatePercentageOf(
+        uint amount,
+        uint percentage
+    ) internal pure returns (uint) {
+        return
+            ABDKMathQuad.toUInt(
+                ABDKMathQuad.div(
+                    ABDKMathQuad.mul(
+                        ABDKMathQuad.fromUInt(amount),
+                        ABDKMathQuad.fromUInt(percentage)
+                    ),
+                    ABDKMathQuad.fromUInt(100)
+                )
+            );
+    }
+
+    /**
+     * @dev Funds the contract with ERC20 tokens
+     * @notice Only the owner can call this function
+     * @param _amount Amount of tokens to fund the contract with
+     */
+    function fundContractWithErc20Token(uint256 _amount) public onlyOwner {
+        Token.transferFrom(msg.sender, address(this), _amount);
+    }
+
+    /**
+     * @dev Calculates the available rewards in the contract
+     * by subtracting the total staked amount from the
+     * total balance of the contract
+     * @return The available rewards in the contract
+     */
+    function getAvailableRewards() public view returns (uint256) {
+        return Token.balanceOf(address(this)).sub(totalStaked);
+    }
+
+    /**
+     * @dev Returns the total staked amount
+     * @return The total staked amount
+     */
+    function getStakedAmount() public view returns (uint256) {
+        return totalStaked;
+    }
+
+    /**
+     * @dev Returns the staked amount for a given address
+     * @param _address The address to get the staked amount for
+     * @return The staked amount for the given address
+     */
+    function getStakedAmountFor(
+        address _address
+    ) public view returns (uint256) {
+        return stakers[_address].stakedAmount;
     }
 }
